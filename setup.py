3fadd6d89909f6fde2b3a0cd4a9a911c65950726
Srikanth Bemineni <srikanth.bemineni@gmail.com>
2017-03-18 15:52:58 -0500

    Setup changes

--- setup.py
+++ setup.py
@@ -12,26 +12,26 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 
 setup(
-    name='edm',
-    version='1.0.0a1',
+    name='eldam',
+    version='1.0.0',
     description="Elastic Search transaction based data manager.",
-    long_description="Elastic Search data manger with transaction support. *****SAMMY***** 133740 wants to put his name",
-    url="www.vertexdblib.com",
+    long_description="Elastic Search data manger with zope transaction support. *****SAMMY***** 133740 wants to put his name",
+    url="https://github.com/bemineni/edm",
     author='Srikanth Bemineni',
     author_email='srikanth.bemineni@gmail.com',
-    license='EDM license',
+    license='MIT',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
             # How mature is this project? Common values are
             #   3 - Alpha
             #   4 - Beta
             #   5 - Production/Stable
-           'Development Status :: 3 - Alpha',
+           'Development Status :: 5 - Production/Stable',
             # Indicate who your project is intended for
            'Intended Audience :: Developers',
            'Topic :: Software Development :: Database library',
            # Pick your license as you wish (should match "license" above)
-           'License :: VertexDB License',
+           'License :: MIT License',
            # Specify the Python versions you support here. In particular, ensure
            # that you indicate whether you support Python 2, Python 3 or both.
           'Programming Language :: Python :: 2.7',
